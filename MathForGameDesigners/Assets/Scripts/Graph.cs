28175f1b186a9c9207266bbc8cd0696485aba7b4
cgbNYU <cgb316@nyu.edu>
2019-02-05 10:51:29 -0500

    Added Graphers from the sample project

---
+++ MathForGameDesigners/Assets/Scripts/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    public Transform pointPrefab;
+    [Range(10, 100)] public int resolution = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = new Vector3(0, 0, 0);
+        for (int i = 0; i < resolution; i++) 
+        {
+            
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
